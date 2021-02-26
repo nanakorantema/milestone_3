887a41e008ed2440c077907a1cf3d45987a7d416
Your Name <nanakorantema_koranteng@fas.harvard.edu>
2021-02-25 20:23:13 -0500

    initial commit

---
+++ Milestone_3/app.R
@@ -0,0 +1,42 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidycensus)
+library(tidyverse)
+library(shinythemes)
+
+# Define UI for application 
+ui <- navbarPage(
+    "Coups and Democracy in the Middle East",
+    tabPanel("Discussion",
+             titlePanel("Project Data"),
+             h3("Sourcing My Data"),
+             p("For my project I plan to look at two data sets. I will first look at coup data collected 
+                by THe Cline Center for Advanced Social Resaerch. Their data set includes information
+                on coups, attempted coups, and coup plots/conspiracies in 136 countries from 1945-2019.
+                that data specifies the type of actor(s) who initiated the coup plots and information 
+                regarding what happened to the deposed leader in each case. Being that the so called 
+                Arab Spring has served as a catalyst for civil resistance and political reform in the Middle
+                East, I am hoping to use this data to compare coup attempts in the region and the 
+                rest of the world. 
+                Next, I plan to use an Arab barometer data set entitled Democracy in the Middle East 
+                and North Africa: Five Years after the Arab Uprisings which explores opinions towards
+                democracy in selected MENA countries. Essentially, I hope to determine whether coups 
+                
+               ")),
+    tabPanel("About", 
+             titlePanel("About"),
+             h3("Project Background and Motivations"),
+             p("Hello, this is a shiny app that I have created for milestone #3 of my big data course"),
+             h3("About Me"),
+             p("My name is Nana-Korantema Koranteng and I study the Middle East. 
+             You can reach me at nanakorantema_koranteng@g.harvard.edu.
+             Your can find the repo for this Milestone here: https://github.com/nanakorantema/milestone_3")))
+  
