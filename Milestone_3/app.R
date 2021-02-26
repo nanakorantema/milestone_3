590e3dfb0228c4f8c88359f7abe541ea7d047282
Your Name <nanakorantema_koranteng@fas.harvard.edu>
2021-02-25 21:58:58 -0500

    completed milestone

--- Milestone_3/app.R
+++ Milestone_3/app.R
@@ -13,7 +13,7 @@ library(tidyverse)
 library(shinythemes)
 
 # Define UI for application 
-ui <- navbarPage(
+ui <- navbarPage(theme = shinytheme("cyborg"),
     "Coups and Democracy in the Middle East",
     tabPanel("Discussion",
              titlePanel("Project Data"),
@@ -28,9 +28,12 @@ ui <- navbarPage(
                 rest of the world. 
                 Next, I plan to use an Arab barometer data set entitled Democracy in the Middle East 
                 and North Africa: Five Years after the Arab Uprisings which explores opinions towards
-                democracy in selected MENA countries. Essentially, I hope to determine whether coups 
-                
-               ")),
+                democracy in selected MENA countries. Essentially, I hope to look for some type of relationship
+                between coup attempts in the region and feelings toward democracy. I will deinfitely need to take
+                my time to go through the codebook for this set as the raw survey data uses many codes and scales.
+                The direction of my project might change (and probably will as I take a closer look at the data)
+                but I am excited about using two different types of data sets that will provide a meta and micro 
+                level study of a region I am deeply interested in.")),
     tabPanel("About", 
              titlePanel("About"),
              h3("Project Background and Motivations"),
@@ -39,4 +42,12 @@ ui <- navbarPage(
              p("My name is Nana-Korantema Koranteng and I study the Middle East. 
              You can reach me at nanakorantema_koranteng@g.harvard.edu.
              Your can find the repo for this Milestone here: https://github.com/nanakorantema/milestone_3")))
-  
+
+# Define server logic required to draw a histogram
+
+server <- function(input, output) {
+    
+}
+# Run the application 
+shinyApp(ui, server)
+runApp("~/shinyapp")
